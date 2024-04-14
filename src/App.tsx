9c59da3bc51f259117958c03a6d5f846d1333592
Angule Mathias <angulemathias3@gmail.com>
2024-04-14 18:18:38 +0300

    set up redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+
+import { AppDispatch, RootState } from "./app/store";
+import { fetchRandomMovies } from "./app/features/movie/movieAPI";
 
 const App: FC = () => {
-  return <div className="App">Welcome</div>;
+  const dispatch: AppDispatch = useDispatch();
+  const movies = useSelector((state: RootState) => state.movie.entities);
+  const loading = useSelector((state: RootState) => state.movie.loading);
+
+  useEffect(() => {
+    dispatch(fetchRandomMovies());
+  }, [dispatch]);
+
+  if (loading === "pending") {
+    return <div>Loading...</div>;
+  }
+
+  if (movies.length === 0) {
+    return <div>No movies found.</div>;
+  }
+
+  console.log(movies[0]);
+
+  return (
+    <div>
+      {movies.map((movie, index) => (
+        <div key={index}>
+          <h2>{movie["#TITLE"]}</h2>
+          <p>{movie["#YEAR"]}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default App;
--- src/app/features/movie/movieAPI.ts
+++ src/app/features/movie/movieAPI.ts
@@ -1,10 +1,12 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { getRandomMovies } from "ma-movies-sdk";
+import { getRandomMovies } from "../../../sdk/src/movies/movieSdk";
 
 export const fetchRandomMovies = createAsyncThunk(
   "movies/fetchRandom",
   async () => {
     const response = await getRandomMovies();
+    console.log("response", response)
     return response.results;
   }
 );
+    
